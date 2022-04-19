c47d719ef008762c21e943c5ecf3c1f5ddd25c7c
alexeyrozhkov <alex31inq@gmail.com>
2022-04-19 23:21:18 +0300

    Добавляет стейт машину

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -4,14 +4,61 @@ import { Modal } from 'antd';
 import { Icon } from '../../components/Icon/Icon';
 import ReactPlayer from 'react-player'
 import styles from './Page.module.css';
+import { createMachine } from "xstate";
+import { useMachine } from '@xstate/react';
 
 const videoUrl = "https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8";
+enum PlayerState {
+    FULL = "FULL",
+    MINI = "MINI"
+}
+
+const machine = createMachine(
+    {
+      id: "player",
+      initial: PlayerState.FULL,
+      states: {
+        [PlayerState.MINI]: {
+          meta: {
+            description: "The full-screen video"
+          },
+          on: {
+            toggle: PlayerState.FULL
+          }
+        },
+        [PlayerState.FULL]: {
+          entry: "playVideo",
+          exit: "pauseVideo",
+          meta: {
+            description: "Mini video"
+          },
+          on: {
+            toggle: PlayerState.MINI,
+            "key.escape": PlayerState.MINI,
+            "video.ended": PlayerState.MINI
+          }
+        }
+      }
+    },
+    {
+      actions: {
+        playVideo: () => {
+          console.log("play");
+        },
+        pauseVideo: () => {
+            console.log("pause");
+        }
+      }
+    }
+);
 
 export function Page() {
     const [isModalVisible, setIsModalVisible] = useState(false);
     const showModal = useCallback(() => {
         setIsModalVisible(true);
-    }, [])
+    }, []);
+    const [state, send] = useMachine(machine);
+
     
     return (
         <div className="app">
@@ -25,8 +72,9 @@ export function Page() {
                     visible={isModalVisible}
                     okText={<Icon className={styles.img} name="playpause"/>}
                     cancelText={<Icon className={styles.img} name="playpause"/>}
+                    className={styles.full}
                 >
-                    <ReactPlayer  url={videoUrl} />
+                    <ReactPlayer url={videoUrl} />
                 </Modal>
             }
         </div>
